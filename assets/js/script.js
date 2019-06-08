88bd628279d894141408eb62318a174a34bb6666
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-06-08 21:36:31 +0530

    Added animationd

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,30 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+$('#s2').hide();
+$('#s3').hide();
+$('#s4').hide();
+
 let i=0;
 setInterval(() => {
-  if(!$('#s1').hasClass('is-hidden')) {
-    $('#s1').addClass('is-hidden');
-    $('#s2').removeClass('is-hidden');
+  if($('#s1').css("display") != "none") {
+    $( '#s1' ).fadeOut('500', () => {
+      $('#s2').fadeIn('500');
+    });
   }
-  else if(!$('#s2').hasClass('is-hidden')) {
-    $('#s2').addClass('is-hidden');
-    $('#s3').removeClass('is-hidden');
+  else if( $('#s2').css("display") != "none") {
+    $( '#s2' ).fadeOut('500', () => {
+      $('#s3').fadeIn('500');
+    });
   }
-  else if(!$('#s3').hasClass('is-hidden')) {
-    $('#s3').addClass('is-hidden');
-    $('#s4').removeClass('is-hidden');
+  else if($('#s3').css("display") != "none") {
+    $( '#s3' ).fadeOut('500', () => {
+      $('#s4').fadeIn('500');
+    });
   }
-  else if(!$('#s4').hasClass('is-hidden')) {
-    $('#s4').addClass('is-hidden');
-    $('#s1').removeClass('is-hidden');
+  else if($('#s4').css("display") != "none") {
+    $( '#s4' ).fadeOut('500', () => {
+      $('#s1').fadeIn('500');
+    });
   }
-},1000);
+},2000);
