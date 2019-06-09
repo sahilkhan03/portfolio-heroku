8184a2c99044e2f85399ffb51e5fbc0a4814011c
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-06-09 12:40:01 +0530

    Added Projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
-    });
-  }
+$(document).ready(function() {
+  $(".navbar-burger").click(function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+  });
 });
 
 $('#s2').hide();
